2295808df5919d6d13ecad5c5413ffa3e2b405e2
jagadishkatam <jagadish.katam@gmail.com>
2024-11-27 17:46:40 -0500

    updates added"

--- world_phuse_map_2025.R
+++ world_phuse_map_2025.R
@@ -118,7 +118,7 @@ mymap <- leaflet(locations) |>
     color = ~ifelse(str_detect(conference,'Pharma'),'blue','red')
   ) |> 
   addControl(
-    "<h4 style='color: darkblue; text-align=center; '>Leaflet Map of Phuse and PharmaSUG Events in 2025</h4>", 
+    "<h4 style='color: darkblue; text-align=center; '>Map of Phuse and PharmaSUG Conferences in the Calender 2025</h4>", 
     position = "topright"
   )
 
